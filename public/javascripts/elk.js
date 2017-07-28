141e46ed71f0ddd838533b7853a976811e680347
Melissa Franzen Desk <melissafranzen@Melissas-iMac.domain>
2017-07-28 15:50:01 -0500

    websert complet

--- public/javascripts/elk.js
+++
@@ -1,9 +0,0 @@
-setInterval(annoy, 60000);
-
-function annoy() {
-  var audio = new Audio('/sounds/elk.mp3');
-  audio.play();
-}
-
-var button = document.getElementById("start_noise")
-button.addEventListener("click", annoy)
